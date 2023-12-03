fd95fc4aac86db69de5071418e79e61a1f26e422
MonkeyareDragon <githubforme73@gmail.com>
2023-12-03 17:38:24 +0545

    Splash Animation Screen is added

--- Login&SignUp/Front-End/loginsignup/lib/main.dart
+++ Login&SignUp/Front-End/loginsignup/lib/main.dart
@@ -1,31 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:page_transition/page_transition.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MyHomePage(),
-    );
-  }
+	
+// This widget is the root of your application.
+@override
+Widget build(BuildContext context) {
+	return MaterialApp(
+		
+	// Application name
+	title: 'FitStar',
+		
+	// Application theme data, you can set the
+	// colors for the application as
+	// you want
+	theme: ThemeData(
+		primarySwatch: Colors.blue,
+	),
+		
+	// A widget which will be started on application startup
+	home: AnimatedSplashScreen(
+		splash: 'assets/SplashScreen/SplashScreenImage.png',
+		nextScreen: MyHomePage(title: 'FitStar'),
+    splashIconSize: 150,
+    duration: 2500,
+		splashTransition: SplashTransition.fadeTransition,
+    pageTransitionType: PageTransitionType.fade,
+	),
+	);
+}
 }
 
 class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('My Flutter App'),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, Flutter!',
-          style: TextStyle(fontSize: 24),
-        ),
-      ),
-    );
-  }
+final String title;
+
+const MyHomePage({required this.title});
+
+@override
+Widget build(BuildContext context) {
+	return Scaffold(
+	appBar: AppBar(
+		
+		// The title text which will be shown on the action bar
+		title: Text(title),
+	),
+	body: Center(
+		child: Text(
+		'Test',
+		),
+	),
+	);
+}
 }
