c00c02493d0d95ec617a01ebbc1abd310fa6f00f
MonkeyareDragon <githubforme73@gmail.com>
2024-04-23 20:02:10 +0545

    Add model traning files.

--- fitnestx/fitnestx/meal/apps.py
+++ fitnestx/fitnestx/meal/apps.py
@@ -4,7 +4,7 @@ class ModConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "fitnestx.meal"
     
-    # def ready(self):
-    #     from fitnestx.meal.meal_scheduler import meal_scheduler_checker
-    #     print("Starting Meal Scheduler ...")
-    #     meal_scheduler_checker.start()
+    def ready(self):
+        from fitnestx.meal.meal_scheduler import meal_scheduler_checker
+        print("Starting Meal Scheduler ...")
+        meal_scheduler_checker.start()
